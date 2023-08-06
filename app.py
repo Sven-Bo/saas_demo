b62fe7c2df0765767b495642519797802caada82
Sven <67136317+Sven-Bo@users.noreply.github.com>
2023-08-06 14:14:30 +0200

    Update

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ add_auth(required=True)
 # The email and subscription status is stored in session state.
 st.write(f"Subscription Status: {st.session_state.user_subscribed}")
 st.write("🎉 Yay! You're all set and subscribed! 🎉")
-st.write(f'By the way, your email is {st.session_state.email}... Safe with us! 🤫')
+st.write(f'By the way, your email is: {st.session_state.email}')
