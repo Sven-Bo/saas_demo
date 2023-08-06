ad3842c3f81315555ebcc51dbcbe46a80d861be9
Sven <67136317+Sven-Bo@users.noreply.github.com>
2023-08-06 13:52:44 +0200

    Give me that update

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from st_paywall import add_auth
 st.set_page_config(layout="wide")
 st.title("The Most Fantabulous SaaS Ever! 🚀")
 
-is_authenticated = add_auth(required=True)
-st.write(is_authenticated)
+add_auth(required=True)
 
-# After authentication, the email and subscription status is stored in session state.
+# ONLY AFTER THE AUTHENTICATION, THE USER WILL SEE THIS ⤵
+# The email and subscription status is stored in session state.
 if st.session_state.user_subscribed:
     st.write("🎉 Yay! You're all set and subscribed! 🎉")
     st.write(f'By the way, your email is {st.session_state.email}... Safe with us! 🤫')
